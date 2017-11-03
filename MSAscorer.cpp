a953953d2da078bb57e29c26b54293d6852558a0
simon.whelan.evolution <simon.whelan.evolution@gmail.com>
2017-11-03 10:33:04 +0100

    More code commenting and help added

--- MSAscorer.cpp
+++ MSAscorer.cpp
@@ -55,8 +55,34 @@ int CountTP(vector <tuple<int,int> > &x);
 int main(int argc, char * argv[]) {
 	SScore score;
 
+	if(argc > 1) {
+		if(strcmp(argv[1], "-h") == 0) {
+			cout << "\n===================================================================";
+			cout << "\n\tMSAscorer : written by Simon Whelan";
+			cout << "\n===================================================================";
+			cout << "\n\nUsage: msascorer TestMSA RefMSA";
+			cout << "\n\nTestMSA.fas / RefMSA.fas can be in FASTA/MSF/Phylip/Interleaved format";
+			cout << "\n\nResults will look like this:\n";
+			cout << "\n#Comparing TestMSA.fas (seq:4;l=112) => REF RefMSA.fas(seq:4;l=78)";
+			cout << "\n#TruePos       FalsePos       FalseNeg       totalRef ";
+			cout << "\n346            59             110            456";
+			cout << "\n\nThe first line (commented with #) is a header for confirming input";
+			cout << "\nThe second line references what returned values. They are defined as follows:";
+			cout << "\n\tTruePos  :  Number of homologous pairs in both the RefMSA found in TestMSA";
+			cout << "\n\tFalsePos :  Number of incorrect identified pairs of characters from the RefMSA found in TestMSA";
+			cout << "\n\t(TruePos + FalsePos = Positives = Number of pairs in TestMSA with characters from only the RefMSA.)";
+			cout << "\n\tFalseNeg :  Number of homologous pairs in RefMSA not found in TestMSA;";
+			cout << "\n\ttotalRef :  Total number of homologous pairs in RefMSA (TruePos + FalseNeg)";
+			cout << "\nNOTE: TrueNeg not included because it's the set of all pairs that could be obtained from RefMSA characters";
+			cout << "\n\nThe final line are the values you need to use for computing available confusion matrix measures\n\n";
+			exit(-1);
+		}
+	}
 	if(argc < 3) {
-		cout << "\nUsage: msascorer testMSA referenceMSA \n\n";
+		cout << "\n===================================================================";
+		cout << "\n\tMSAscorer : written by Simon Whelan";
+		cout << "\n===================================================================";
+		cout << "\n\nStandard usage: msascorer TestMSA RefMSA\n\nType \"./msascorer -h\" for help\n\n";
 		exit(-1);
 	}
 	// Options
@@ -105,7 +131,7 @@ int main(int argc, char * argv[]) {
 // Compare the pairs x and y <0: test, 1: ref> and return the score
 SScore ComparePairs(tuple<string, string> seq1, tuple<string, string> seq2) {
 	SScore retScore;
-	// The pair of sequences referenced by position in y ; -1 used to denote gap or something not counted in x
+	// The pair of sequences referenced by position in y
 	tuple<vector<int>,vector<int>> s1_int = MapPositions(get<0>(seq1),get<1>(seq1),get<1>(seq2));
 	tuple<vector<int>,vector<int>> s2_int = MapPositions(get<0>(seq2),get<1>(seq2),get<1>(seq1));
 	vector <tuple<int,int> > testPairs = MakePairs(get<0>(s1_int),get<0>(s2_int));
@@ -114,35 +140,41 @@ SScore ComparePairs(tuple<string, string> seq1, tuple<string, string> seq2) {
 	retScore.totalTest = testPairs.size();
 	retScore.totalRef = refPairs.size();
 	retScore.TP = CountTP(testPairs);
-	retScore.FN = retScore.totalRef - retScore.TP;
-	retScore.FP = testPairs.size() - retScore.TP;
+	retScore.FN = retScore.totalRef - retScore.TP;		// FNs are the
+	retScore.FP = testPairs.size() - retScore.TP;		// FPs are the size of testPairs that are not TP
 	assert(retScore.FN >= 0);
 	return retScore;
 }
 
 // Map the reference sequence y onto the test sequence x. Uses the second reference sequence z to ensure gaps are handled correctly
+// ---
+// Each character in the pair of sequence x,y (returned in tuple<vector<int>,vector<int>>) is labelled as follows:
+// -BIG_NUMBER : Not in reference MSA (only x; all of y will be labelled)
+// -int : In the reference, but aligned to a gap
+// int : In the reference and aligned to a real character
+// The logic is commented into the code
 tuple<vector<int>,vector<int>> MapPositions(string &x, string &y, string &z) {
-	string x_clean = RemoveGaps(x);
+	string x_clean = RemoveGaps(x);	// The raw sequences unaligned
 	string y_clean = RemoveGaps(y);
-	assert(y.size() == z.size());
-	vector <int> y_clean_int(y_clean.size(),-1); 				// The raw sequences unaligned
-	vector <int> x_int(x.size(),-1), y_int(y.size(),-1);		// The return
-	int start_x = (int) x_clean.find(y_clean);	// Get the position recast as int
-	int end_x = start_x + y_clean.size();
+	assert(y.size() == z.size());	// Check the aligned reference is correct
+	vector <int> y_clean_int(y_clean.size(),-BIG_NUMBER);	// Storage of character mapping
+	vector <int> x_int(x.size(),-1), y_int(y.size(),-BIG_NUMBER);		// The return vectors
+	int start_x = (int) x_clean.find(y_clean);	// Get the real character position that y starts in clean_x, recast as int
+	int end_x = start_x + y_clean.size();		// Get the positon in clean_x where y is expected to end
 	if(start_x == string::npos) { cout << "\nError: reference sequence is not a valid subset of the test sequence\ntest: " << x << "\nref:  " << y; exit(-1); }
 	// Create the map for reference (y) based on the other reference (z)
-	int pos = 0;
+	int pos = 0;										// Sequence position number (observable character)
 	for(int i = 0; i < y.size(); i++) {
-		if(IsGap(z[i])) { y_int[i] = -i; }
-		else { y_int[i] = i; }
-		if(!IsGap(y[i])) { y_clean_int[pos++] = i; }
+		if(IsGap(z[i])) { y_int[i] = -i; }				// If aligned to a gap => label as -int
+		else { y_int[i] = i; }							// If aligned to a character => label as int
+		if(!IsGap(y[i])) { y_clean_int[pos++] = i; }    // Transfer that number to y_clean_int for mapping to x
 	}
 	// Now build the x_int
-	pos = 0;
+	pos = 0;											// Sequence position number (observable character)
 	for(int i = 0; i < x.size(); i++) {
-		if(IsGap(x[i])) { continue; }
-		if(pos < start_x || pos >= end_x) { pos++; continue; }
-		x_int[i] = y_clean_int[pos++ - start_x];
+		if(IsGap(x[i])) { continue; }							// Don't care about gaps
+		if(pos < start_x || pos >= end_x) { pos++; continue; }	// If the observable character in x is not also in y, just skip
+		x_int[i] = y_clean_int[pos++ - start_x];				// Otherwise add the label from y_clean_int to x_int
 	}
 	return tuple<vector<int>,vector<int>>(x_int,y_int);
 }
